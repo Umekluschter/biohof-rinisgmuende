684c788649edde7b8116e7356cf538895851d9f8
Josias Buchli <josias.buchli@gmx.ch>
2024-04-28 15:06:32 +0200

    feat: Home finished + Header collapsing

--- src/framework/gmuende-header/gmuende-header.component.ts
+++ src/framework/gmuende-header/gmuende-header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, HostBinding, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { combineLatest, debounceTime, distinctUntilChanged, filter, fromEvent, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'gmuende-header',
@@ -8,4 +10,26 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './gmuende-header.component.html',
   styleUrl: './gmuende-header.component.scss'
 })
-export class GmuendeHeaderComponent { }
+export class GmuendeHeaderComponent {
+  public collapsed = false;
+
+  @HostBinding('class.collapsed') get _collapsed() {
+    return this.collapsed;
+  }
+
+  public constructor() {
+    const scrollEvent = fromEvent(window, 'scroll', { capture: true }).pipe(
+      map(() => document.documentElement.scrollTop > 50),
+      takeUntilDestroyed(),
+    );
+
+    const mouseEvent = fromEvent<MouseEvent>(window, 'mousemove').pipe(
+      map((event) => event.clientY > 100),
+      takeUntilDestroyed(),
+    );
+
+    combineLatest([scrollEvent, mouseEvent]).pipe(
+      takeUntilDestroyed(),
+    ).subscribe(([scroll, mouse]) => this.collapsed = scroll && mouse);
+  }
+}
